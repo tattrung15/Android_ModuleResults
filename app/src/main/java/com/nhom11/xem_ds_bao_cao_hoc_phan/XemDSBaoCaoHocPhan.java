c97684f1a26c256557a649deed1e77d18714ac80
lenam <lenam.opentechiz@gmail.com>
2021-08-20 16:43:18 +0700

    doing custom ListView

--- app/src/main/java/com/nhom11/xem_ds_bao_cao_hoc_phan/XemDSBaoCaoHocPhan.java
+++ app/src/main/java/com/nhom11/xem_ds_bao_cao_hoc_phan/XemDSBaoCaoHocPhan.java
@@ -2,16 +2,30 @@ package com.nhom11.xem_ds_bao_cao_hoc_phan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.*;
 import android.os.Bundle;
+
 import com.nhom11.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class XemDSBaoCaoHocPhan extends AppCompatActivity {
+    MaterialToolbar topAppBar = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xem_ds_bao_cao_hoc_phan);
+        getElements();
+        setListeners();
+    }
 
+    protected void getElements() {
+        topAppBar = findViewById(R.id.topAppBar);
+    }
+
+    protected void setListeners() {
+        topAppBar.setNavigationOnClickListener(v -> {
+            finish();
+        });
     }
 }
