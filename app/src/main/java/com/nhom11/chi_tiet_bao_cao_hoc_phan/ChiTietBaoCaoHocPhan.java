366daf3359060e5478dd6358c7908220cd7aa798
Tất Trung <buitrungt@gmail.com>
2021-08-23 17:21:10 +0700

    edit chi tiet bao cao hoc phan

--- app/src/main/java/com/nhom11/chi_tiet_bao_cao_hoc_phan/ChiTietBaoCaoHocPhan.java
+++ app/src/main/java/com/nhom11/chi_tiet_bao_cao_hoc_phan/ChiTietBaoCaoHocPhan.java
@@ -1,11 +1,16 @@
 package com.nhom11.chi_tiet_bao_cao_hoc_phan;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,6 +19,7 @@ import com.google.android.material.appbar.MaterialToolbar;
 import com.nhom11.R;
 import com.nhom11.database.MyDatabaseHelper;
 import com.nhom11.dto.BaoCaoHocPhanDTO;
+import com.nhom11.models.BaoCaoHocPhan;
 import com.nhom11.models.HocPhan;
 import com.nhom11.utils.CustomAlertDialog;
 
@@ -22,12 +28,16 @@ import java.util.ArrayList;
 public class ChiTietBaoCaoHocPhan extends AppCompatActivity {
 
     ListView listView;
+    EditText editBCHPTongLop, editBCHPSoGio;
+    Button btnBCHP_Them, btnBCHP_Sua;
     MaterialToolbar topAppBar;
     CustomListview adapterBCHP = null;
     CustomSpinner adapterHocPhan = null;
     ArrayList<BaoCaoHocPhanDTO> baoCaoHocPhanDTOs = null;
     ArrayList<HocPhan> hocPhans = null;
+    ArrayList<String> arrLoaiHocPhan = null;
     Spinner spinnerLoaiHP = null, spinnerHP = null;
+    BaoCaoHocPhanDTO baoCaoHocPhanDTO = null;
 
     MyDatabaseHelper databaseHelper = null;
 
@@ -42,9 +52,56 @@ public class ChiTietBaoCaoHocPhan extends AppCompatActivity {
         setListeners();
         setSpinnerHocPhan();
 
+        Intent intent = getIntent();
+        if (!intent.hasExtra("baoCaoHocPhanDTO")) {
+            initListView();
+        } else {
+            btnBCHP_Them.setEnabled(false);
+            btnBCHP_Them.setClickable(false);
+            baoCaoHocPhanDTO = intent.getParcelableExtra("baoCaoHocPhanDTO");
+            initListViewFromIntent();
+            fillForm();
+        }
+    }
+
+    private void fillForm() {
+        int positionSpinnerHP = 0;
+        for (int i = 0; i < hocPhans.size(); i++) {
+            if (hocPhans.get(i).getMaHocPhan().compareTo(baoCaoHocPhanDTO.getMaHocPhan()) == 0) {
+                positionSpinnerHP = i;
+                break;
+            }
+        }
+        spinnerHP.setSelection(positionSpinnerHP);
+        spinnerHP.setEnabled(false);
+        spinnerHP.setClickable(false);
+
+        int positionSpinnerLHP = 0;
+        for (int i = 0; i < arrLoaiHocPhan.size(); i++) {
+            if (arrLoaiHocPhan.get(i).compareTo(baoCaoHocPhanDTO.getLoaiHocPhan()) == 0) {
+                positionSpinnerLHP = i;
+                break;
+            }
+        }
+        spinnerLoaiHP.setSelection(positionSpinnerLHP);
+
+        editBCHPTongLop.setText(String.valueOf(baoCaoHocPhanDTO.getTongSoLop()));
+        editBCHPSoGio.setText(String.valueOf(baoCaoHocPhanDTO.getTongSoGio()));
+    }
+
+    private void initListView() {
         baoCaoHocPhanDTOs = (ArrayList<BaoCaoHocPhanDTO>) databaseHelper.getAllBaoCaoHP();
         adapterBCHP = new CustomListview(this, R.layout.bao_cao_hp_custom_listview, baoCaoHocPhanDTOs);
         listView.setAdapter(adapterBCHP);
+    }
+
+    private void initListViewFromIntent() {
+        baoCaoHocPhanDTOs = new ArrayList<>();
+        baoCaoHocPhanDTOs.add(baoCaoHocPhanDTO);
+        adapterBCHP = new CustomListview(this, R.layout.bao_cao_hp_custom_listview, baoCaoHocPhanDTOs);
+        listView.setAdapter(adapterBCHP);
+        listView.setEnabled(false);
+        listView.setClickable(false);
     }
 
     private void setSpinnerHocPhan() {
@@ -59,12 +116,16 @@ public class ChiTietBaoCaoHocPhan extends AppCompatActivity {
         spinnerLoaiHP = findViewById(R.id.spinnerBCHP_LoaiHP);
         spinnerHP = findViewById(R.id.spinnerBCHP_HP);
         listView = findViewById(R.id.lvBCHP_BaoCaoGiangDay);
+        btnBCHP_Them = findViewById(R.id.btnBCHP_Them);
+        btnBCHP_Sua = findViewById(R.id.btnBCHP_Sua);
+        editBCHPTongLop = findViewById(R.id.editBCHPTongLop);
+        editBCHPSoGio = findViewById(R.id.editBCHPSoGio);
 
-        ArrayList<String> arrLHP = new ArrayList<>();
-        arrLHP.add("Bắt buộc");
-        arrLHP.add("Tự chọn");
+        arrLoaiHocPhan = new ArrayList<>();
+        arrLoaiHocPhan.add("Bắt buộc");
+        arrLoaiHocPhan.add("Tự chọn");
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, arrLHP);
+                android.R.layout.simple_spinner_item, arrLoaiHocPhan);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerLoaiHP.setAdapter(arrayAdapter);
     }
@@ -83,6 +144,43 @@ public class ChiTietBaoCaoHocPhan extends AppCompatActivity {
                     return true;
                 }
                 return false;
+            }
+        });
+
+        btnBCHP_Sua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (baoCaoHocPhanDTO != null) {
+
+                    if (editBCHPTongLop.getText().toString().compareTo("") == 0) {
+                        Toast.makeText(getBaseContext(), "Tổng số lớp không được để trống",
+                                Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (editBCHPSoGio.getText().toString().compareTo("") == 0) {
+                        Toast.makeText(getBaseContext(), "Tổng số giờ không được để trống",
+                                Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    baoCaoHocPhanDTO.setTongSoLop(Integer.parseInt(editBCHPTongLop.getText().toString()));
+                    baoCaoHocPhanDTO.setTongSoGio(Float.parseFloat(editBCHPSoGio.getText().toString()));
+                    baoCaoHocPhanDTO.setLoaiHocPhan(spinnerLoaiHP.getSelectedItem().toString());
+
+                    BaoCaoHocPhan baoCaoHocPhan = new BaoCaoHocPhan(baoCaoHocPhanDTO.getMaBaoCaoHocPhan(),
+                            baoCaoHocPhanDTO.getMaHocPhan(), baoCaoHocPhanDTO.getTongSoLop(),
+                            baoCaoHocPhanDTO.getTongSoGio(), baoCaoHocPhanDTO.getLoaiHocPhan());
+                    int rowEffect = databaseHelper.updateBaoCaoHocPhan(baoCaoHocPhan);
+                    if (rowEffect >= 0) {
+                        Toast.makeText(getApplicationContext(), "Cập nhập thành công",
+                                Toast.LENGTH_SHORT).show();
+                        adapterBCHP.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Cập nhập thất bại",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
